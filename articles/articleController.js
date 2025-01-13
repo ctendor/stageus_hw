3947dbaf37011038efb358231939c8605e18ddcd
TK <ideonec@naver.com>
2025-01-13 19:54:42 +0900

    feat article 예외처리 추가

--- articles/articleController.js
+++ articles/articleController.js
@@ -1,47 +1,142 @@
+const {
+  addArticle,
+  findArticleByIdx,
+  updateArticle,
+  deleteArticle,
+  getArticles,
+  likeArticle,
+  unlikeArticle,
+} = require("../repositories/articlesRepository");
+const customError = require("../utils/customError");
 const asyncWrapper = require("../utils/asyncWrapper");
-const articleService = require("./articleService");
+const { idxRegx, titleRegx, categoryRegx } = require("../constants/regx");
 
 const createArticle = asyncWrapper(async (req, res) => {
   const { title, content, category } = req.body;
-  const newArticle = await articleService.createArticle({
+
+  if (!title || !content || !category) {
+    throw customError("제목, 내용, 카테고리를 모두 입력해주세요.", 400);
+  }
+  if (!titleRegx.test(title)) {
+    throw customError("제목은 1~20자이어야 합니다.", 400);
+  }
+  if (!categoryRegx.test(String(category))) {
+    throw customError("유효하지 않은 카테고리입니다.", 400);
+  }
+
+  const newArticle = addArticle({
     title,
     content,
-    category,
+    category: parseInt(category),
+    createdAt: new Date().toISOString(),
+    updatedAt: new Date().toISOString(),
+    likes: 0,
   });
+
   res.status(201).send({
     message: "게시글이 생성되었습니다.",
     articleIdx: newArticle.articleIdx,
   });
 });
 
-const getArticles = asyncWrapper(async (req, res) => {
-  const articles = await articleService.getArticles(req.query);
-  res.status(200).send({ articles });
-});
-
-const getArticleById = asyncWrapper(async (req, res) => {
+const getArticle = asyncWrapper(async (req, res) => {
   const { id } = req.params;
-  const article = await articleService.getArticleById(id);
-  res.status(200).send({ article });
+
+  if (!idxRegx.test(id)) {
+    throw customError("유효하지 않은 IDX입니다.", 400);
+  }
+
+  const article = findArticleByIdx(parseInt(id));
+  if (!article) {
+    throw customError("게시글을 찾을 수 없습니다.", 404);
+  }
+
+  res.status(200).send(article);
 });
 
 const updateArticle = asyncWrapper(async (req, res) => {
   const { id } = req.params;
   const { title, content, category } = req.body;
-  await articleService.updateArticle(id, { title, content, category });
+
+  if (!idxRegx.test(id)) {
+    throw customError("유효하지 않은 IDX입니다.", 400);
+  }
+  if (title && !titleRegx.test(title)) {
+    throw customError("제목은 1~20자이어야 합니다.", 400);
+  }
+  if (category && !categoryRegx.test(String(category))) {
+    throw customError("유효하지 않은 카테고리입니다.", 400);
+  }
+
+  const article = findArticleByIdx(parseInt(id));
+  if (!article) {
+    throw customError("게시글을 찾을 수 없습니다.", 404);
+  }
+
+  updateArticle(parseInt(id), {
+    title: title || article.title,
+    content: content || article.content,
+    category: category ? parseInt(category) : article.category,
+    updatedAt: new Date().toISOString(),
+  });
+
   res.status(200).send({ message: "게시글이 수정되었습니다." });
 });
 
 const deleteArticle = asyncWrapper(async (req, res) => {
   const { id } = req.params;
-  await articleService.deleteArticle(id);
+
+  if (!idxRegx.test(id)) {
+    throw customError("유효하지 않은 IDX입니다.", 400);
+  }
+
+  const article = findArticleByIdx(parseInt(id));
+  if (!article) {
+    throw customError("게시글을 찾을 수 없습니다.", 404);
+  }
+
+  deleteArticle(parseInt(id));
+
   res.status(200).send({ message: "게시글이 삭제되었습니다." });
+});
+
+const likeArticle = asyncWrapper(async (req, res) => {
+  const { id } = req.params;
+
+  if (!idxRegx.test(id)) {
+    throw customError("유효하지 않은 IDX입니다.", 400);
+  }
+
+  const article = findArticleByIdx(parseInt(id));
+  if (!article) {
+    throw customError("게시글을 찾을 수 없습니다.", 404);
+  }
+
+  const likes = likeArticle(parseInt(id));
+  res.status(200).send({ message: "게시글에 좋아요를 눌렀습니다.", likes });
+});
+
+const unlikeArticle = asyncWrapper(async (req, res) => {
+  const { id } = req.params;
+
+  if (!idxRegx.test(id)) {
+    throw customError("유효하지 않은 IDX입니다.", 400);
+  }
+
+  const article = findArticleByIdx(parseInt(id));
+  if (!article) {
+    throw customError("게시글을 찾을 수 없습니다.", 404);
+  }
+
+  const likes = unlikeArticle(parseInt(id));
+  res.status(200).send({ message: "게시글 좋아요가 취소되었습니다.", likes });
 });
 
 module.exports = {
   createArticle,
-  getArticles,
-  getArticleById,
+  getArticle,
   updateArticle,
   deleteArticle,
+  likeArticle,
+  unlikeArticle,
 };
--- comments/commentsController.js
+++ comments/commentsController.js
@@ -11,17 +11,22 @@ const { findArticleByIdx } = require("../repositories/articlesRepository");
 const customError = require("../utils/customError");
 const { idxRegx } = require("../constants/regx");
 
-const createComment = (req, res) => {
+const createComment = (req, res, next) => {
   try {
     const { articleIdx } = req.params;
-    const { content } = req.body; 
+    const { content } = req.body;
 
-    if (!idxRegx.test(articleIdx))
+    if (!idxRegx.test(articleIdx)) {
       throw customError("유효하지 않은 게시글 IDX입니다.", 400);
-    if (!content) throw customError("댓글 내용을 입력해주세요.", 400);
+    }
+    if (!content || typeof content !== "string") {
+      throw customError("댓글 내용을 입력해주세요.", 400);
+    }
 
     const article = findArticleByIdx(parseInt(articleIdx));
-    if (!article) throw customError("게시글을 찾을 수 없습니다.", 404);
+    if (!article) {
+      throw customError("게시글을 찾을 수 없습니다.", 404);
+    }
 
     const newComment = addComment({
       articleIdx: parseInt(articleIdx),
@@ -36,104 +41,105 @@ const createComment = (req, res) => {
       commentIdx: newComment.commentIdx,
     });
   } catch (err) {
-    console.log(err.message);
-    console.log(err.stack);
-    res.status(err.statusCode || 500).send({
-      message: err.message || "Internal Server Error",
-    });
+    next(err);
   }
 };
 
-const getComment = (req, res) => {
+const getComment = (req, res, next) => {
   try {
     const { articleIdx, commentIdx } = req.params;
-    if (!idxRegx.test(articleIdx) || !idxRegx.test(commentIdx))
+
+    if (!idxRegx.test(articleIdx) || !idxRegx.test(commentIdx)) {
       throw customError("유효하지 않은 IDX입니다.", 400);
+    }
+
     const comment = findCommentByIdx(parseInt(commentIdx));
-    if (!comment || comment.articleIdx !== parseInt(articleIdx))
+    if (!comment || comment.articleIdx !== parseInt(articleIdx)) {
       throw customError("댓글을 찾을 수 없습니다.", 404);
-    res.send(comment);
+    }
+
+    res.status(200).send(comment);
   } catch (err) {
-    console.log(err.message);
-    console.log(err.stack);
-    res.status(err.statusCode || 500).send({
-      message: err.message || "Internal Server Error",
-    });
+    next(err);
   }
 };
 
-const updateCommentController = (req, res) => {
+const updateCommentController = (req, res, next) => {
   try {
     const { articleIdx, commentIdx } = req.params;
     const { content } = req.body;
-    if (!idxRegx.test(articleIdx) || !idxRegx.test(commentIdx))
+
+    if (!idxRegx.test(articleIdx) || !idxRegx.test(commentIdx)) {
       throw customError("유효하지 않은 IDX입니다.", 400);
+    }
+    if (!content || typeof content !== "string") {
+      throw customError("댓글 내용을 입력해주세요.", 400);
+    }
+
     const comment = findCommentByIdx(parseInt(commentIdx));
-    if (!comment || comment.articleIdx !== parseInt(articleIdx))
+    if (!comment || comment.articleIdx !== parseInt(articleIdx)) {
       throw customError("댓글을 찾을 수 없습니다.", 404);
-    if (!content) throw customError("댓글 내용을 입력해주세요.", 400);
-    const updatedComment = updateComment(parseInt(commentIdx), {
+    }
+
+    updateComment(parseInt(commentIdx), {
       content,
       updatedAt: new Date().toISOString(),
     });
-    res.send({ message: "댓글이 수정되었습니다." });
+
+    res.status(200).send({ message: "댓글이 수정되었습니다." });
   } catch (err) {
-    console.log(err.message);
-    console.log(err.stack);
-    res.status(err.statusCode || 500).send({
-      message: err.message || "Internal Server Error",
-    });
+    next(err);
   }
 };
 
-const deleteCommentController = (req, res) => {
+const deleteCommentController = (req, res, next) => {
   try {
     const { articleIdx, commentIdx } = req.params;
-    if (!idxRegx.test(articleIdx) || !idxRegx.test(commentIdx))
+
+    if (!idxRegx.test(articleIdx) || !idxRegx.test(commentIdx)) {
       throw customError("유효하지 않은 IDX입니다.", 400);
+    }
+
     const comment = findCommentByIdx(parseInt(commentIdx));
-    if (!comment || comment.articleIdx !== parseInt(articleIdx))
+    if (!comment || comment.articleIdx !== parseInt(articleIdx)) {
       throw customError("댓글을 찾을 수 없습니다.", 404);
+    }
+
     deleteComment(parseInt(commentIdx));
-    res.send({ message: "댓글이 삭제되었습니다." });
+
+    res.status(200).send({ message: "댓글이 삭제되었습니다." });
   } catch (err) {
-    console.log(err.message);
-    console.log(err.stack);
-    res.status(err.statusCode || 500).send({
-      message: err.message || "Internal Server Error",
-    });
+    next(err);
   }
 };
 
-const likeCommentController = (req, res) => {
+const likeCommentController = (req, res, next) => {
   try {
     const { articleIdx, commentIdx } = req.params;
-    if (!idxRegx.test(articleIdx) || !idxRegx.test(commentIdx))
+
+    if (!idxRegx.test(articleIdx) || !idxRegx.test(commentIdx)) {
       throw customError("유효하지 않은 IDX입니다.", 400);
+    }
+
     const likes = likeComment(parseInt(commentIdx));
-    res.send({ message: "댓글에 좋아요를 눌렀습니다.", likes });
+    res.status(200).send({ message: "댓글에 좋아요를 눌렀습니다.", likes });
   } catch (err) {
-    console.log(err.message);
-    console.log(err.stack);
-    res.status(err.statusCode || 500).send({
-      message: err.message || "Internal Server Error",
-    });
+    next(err);
   }
 };
 
-const unlikeCommentController = (req, res) => {
+const unlikeCommentController = (req, res, next) => {
   try {
     const { articleIdx, commentIdx } = req.params;
-    if (!idxRegx.test(articleIdx) || !idxRegx.test(commentIdx))
+
+    if (!idxRegx.test(articleIdx) || !idxRegx.test(commentIdx)) {
       throw customError("유효하지 않은 IDX입니다.", 400);
+    }
+
     const likes = unlikeComment(parseInt(commentIdx));
-    res.send({ message: "댓글 좋아요가 취소되었습니다.", likes });
+    res.status(200).send({ message: "댓글 좋아요가 취소되었습니다.", likes });
   } catch (err) {
-    console.log(err.message);
-    console.log(err.stack);
-    res.status(err.statusCode || 500).send({
-      message: err.message || "Internal Server Error",
-    });
+    next(err);
   }
 };
 
