8456a176d282ad00883c089687a73606fda3227e
TK <ideonec@naver.com>
2025-01-14 13:39:13 +0900

    feat: error 코드 병합

--- middlewares/errorHandler.js
+++ middlewares/errorHandler.js
@@ -22,4 +22,10 @@ const errorHandler = (err, req, res, next) => {
   }
 };
 
-module.exports = errorHandler;
+const notFoundMiddleware = (req, res, next) => {
+  res.status(404).send({
+    message: "요청하신 API를 찾을 수 없습니다.",
+  });
+};
+
+module.exports = { errorHandler, notFoundMiddleware };
